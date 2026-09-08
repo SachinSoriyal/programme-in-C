935980b7f31f04d9fbd220f29ff278085a9a317e
SachinSoriyal.18 <sachinsingh.11272@gmail.com>
2026-09-08 22:48:29 +0530

    Refactor SACHIN_01.c with comments and new main
    Added comments and a new main function with an integer variable.

--- SACHIN_01.c
+++ SACHIN_01.c
@@ -1,6 +1,16 @@
-// first program of C
+//print name in C
 #include<stdio.h>
 
 int main () {
     printf("sachin singh");
+    return 0;
+}
+
+//  Data types
+// integer (int)
+#include<stdio.h>
+
+int main(){
+    int class = 12;
+    return0;
 }
