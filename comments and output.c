300c5bc2ba33fa13c62c31bc428740491d123654
SachinSoriyal. <sachinsingh.11272@gmail.com>
2026-09-20 15:49:11 +0000

    Update

--- comments and output.c
+++ comments and output.c
@@ -1,1 +1,30 @@
+// comments two type single and multiple line comment 
+// single line comment 
 
+// 02 multiple line comment 
+/*
+sachin singh is my name 
+I am pusrsuing B.tech is cse core 
+I am trying to figuring things out 
+*/
+
+
+// Output 
+// #include<stdio.h>
+
+// int main(){
+//     printf("Hello Sir");
+//     return 0;
+// }
+
+
+// use of /n
+#include<stdio.h>
+
+int main(){
+    printf("Hello Sir\n");
+    printf("Hello Sir\n");
+    printf("Hello Sir\n");
+    printf("Hello Sir");
+    return 0;
+}
